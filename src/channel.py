5d618d1eddb5662b61d3a90caa2c9b7fa24a30ef
Maksim Bolotin <vk07122012@gmail.com>
2023-12-30 19:50:44 +0300

    В класс Channel добавлены методы: __str__, __add__, __sub__, __lt__, __le__, __gt__, __ge__, __eq__

--- src/channel.py
+++ src/channel.py
@@ -47,3 +47,27 @@ class Channel:
         }
         with open("moscowpython.json", "w", encoding="UTF-8") as file:
             dump(result, file, ensure_ascii=False, indent=2)
+
+    def __str__(self):
+        return f"'{self.title} ({self.url})'"
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
